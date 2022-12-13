0db051ba52b036e28d6ac9899a94b8f14d6bd2a6
alirezanqp <alirezanqp@gmail.com>
2022-12-13 14:30:16 +0330

    add globall atGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './utils/logger.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './modules/common/guards/at.guard';
 
 @Module({
   imports: [UserModule, PrismaModule, AuthModule, ConfigModule.forRoot({})],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
