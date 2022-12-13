35af4314b5d17c31d60af84257d6f287e62f420d
alirezanqp <alirezanqp@gmail.com>
2022-12-13 14:30:49 +0330

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as cookieParser from "cookie-parser"
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
