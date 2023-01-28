dfa2b7f576ce529bf3042f139f5bea872b166f8a
khalvai <khalvayozbek@gmail.com>
2023-01-28 12:21:29 -0500

    fixing bug uploads

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(7070);
+  await app.listen(5000);
 }
 bootstrap();
