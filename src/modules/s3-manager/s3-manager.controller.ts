4f4775f0cb976b731e3b19e18cb08dad21d82135
alirezanqp <alirezanqp@gmail.com>
2023-01-31 13:24:02 +0330

    add AWS S3 Manager

---
+++ src/modules/s3-manager/s3-manager.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { S3ManagerService } from './s3-manager.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { File } from '../../common/interfaces/file.interface';
+
+@Controller()
+export class S3ManagerController {
+  constructor(private readonly s3: S3ManagerService) {}
+
+  @Get('buckets')
+  getBuckets(): Promise<any> {
+    return this.s3.listBuckets();
+  }
+
+  @Get('buckets/:bucketName')
+  getObjectsByBucket(@Param('bucketName') bucketName: string): Promise<any> {
+    return this.s3.listObjects(bucketName);
+  }
+
+  @Post('buckets/:bucketName')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @Param('bucketName') bucketName: string,
+    @UploadedFile() file: File,
+  ): Promise<any> {
+    return this.s3.uploadFile(bucketName, file);
+  }
+}
