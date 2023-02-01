2660bd4c2b61b22a4de52256129487da90896075
Alireza Naghdipour <alirezanqp@gmail.com>
2023-02-01 21:14:19 +0330

    َUpdate main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:8000',
+    origin: 'https://noghteh-khat.ir/',
     credentials: true,
   });
 
