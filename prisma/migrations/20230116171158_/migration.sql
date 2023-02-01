78861472ed13f6f47054d87a7b7839a09531f663
alirezanqp <alirezanqp@gmail.com>
2023-02-02 01:10:20 +0330

    remove migrations

--- prisma/migrations/20230116171158_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Professor" ADD COLUMN     "isVerified" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "Subject" ADD COLUMN     "isVerified" BOOLEAN NOT NULL DEFAULT false;
