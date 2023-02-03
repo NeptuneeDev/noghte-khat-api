1fd6374224a81170e247705852221a9357f7b645
khalvai <khalvayozbek@gmail.com>
2023-02-03 05:29:38 -0500

    testing port

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api-docs', app, document);
   }
-  // await app.listen(7070, '0.0.0.0');
-  await app.listen(7070);
+  await app.listen(7070, '0.0.0.0');
+  // await app.listen(7070);
 }
 bootstrap();
