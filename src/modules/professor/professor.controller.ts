a4fbce002d0f4dcee0659665ea3a16686a2abc67
khalvai <khalvayozbek@gmail.com>
2023-02-24 01:52:55 -0500

    correct accept professor url

--- src/modules/professor/professor.controller.ts
+++ src/modules/professor/professor.controller.ts
@@ -62,7 +62,7 @@ export class ProfessorController {
   }
 
   @Roles(Role.Admin)
-  @Get('acceptt/:id')
+  @Get('accept/:id')
   async accept(@Param('id', ParseIntPipe) professorId: number) {
     return await this.professorService.accept(professorId);
   }
