1d9e1e42e61335ed3c5015aef5660e09f556bdb1
khalvai <khalvayozbek@gmail.com>
2023-03-18 13:49:24 -0400

    moving guard into auth

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../types/roles.enum';
+import { Observable } from 'rxjs';
+export const ROLES_KEY = 'roles';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass,
+    ]);
+
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+
+    return requiredRoles.some((role) => user.role?.includes(role));
+  }
+}
