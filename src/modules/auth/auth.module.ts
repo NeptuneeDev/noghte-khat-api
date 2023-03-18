9e602f698dc67c76e6d9a3a93aaef51501a95364
khalvai <khalvayozbek@gmail.com>
2023-03-18 13:41:30 -0400

    auth refactoring

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,39 +3,17 @@ import { userRepository } from '../user/user.repository';
 import { AuthController } from './auth.controller';
 import { AuthRepository } from './auth.repository';
 import { AuthService } from './auth.service';
-import { MailService } from './mail.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './startegies/at.startegy';
 import { RefreshTokenStrategy } from './startegies/rt.strategy';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { join } from 'path';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-
+import { googleStrategy } from './startegies/google.strategy';
+import { Otp } from '../../common/utils/Otp';
+import { MaileModule } from '../mail/mail.module';
+import { MailService } from '../mail/mail.service';
 @Module({
   imports: [
     JwtModule.register({ secret: process.env.SECRET_KEY }),
-    MailerModule.forRootAsync({
-      useFactory: async () => ({
-        transport: {
-          host: process.env.MAIL_HOST,
-          port: +process.env.MAIL_PORT,
-          auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASSWORD,
-          },
-        },
-        defaults: {
-          from: '"Noghteh Khat" <info@mail.noghteh-khat.ir>',
-        },
-        template: {
-          dir: join(__dirname, 'templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
-          },
-        },
-      }),
-    }),
+    MaileModule,
   ],
   providers: [
     AuthService,
@@ -43,7 +21,9 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
     MailService,
     userRepository,
     RefreshTokenStrategy,
+    googleStrategy,
     JwtStrategy,
+    Otp,
   ],
   controllers: [AuthController],
 })
--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PartiQLBatchResponse } from 'aws-sdk/clients/dynamodb';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from './interfaces/user.interface';
 @Injectable()
 export class userRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  find(email: string) {
+  find(email: string): Promise<User | undefined> {
     return this.prisma.user.findFirst({ where: { email: email } });
   }
 
@@ -16,12 +17,36 @@ export class userRepository {
   findUnique(id: number) {
     return this.prisma.user.findUnique({ where: { id: id } });
   }
+  async logOut(userId: number): Promise<boolean> {
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRT: null,
+      },
+    });
+    return true;
+  }
+  async updateRtHash(
+    userId: number,
+    hashedRefreshToken: string,
+  ): Promise<void> {
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRT: hashedRefreshToken,
+      },
+    });
+  }
 
   async upsert(user: Partial<User>): Promise<User> {
     return this.prisma.user.upsert({
       create: {
         name: user.name,
-        password: user.password,
+        password: user?.password,
         email: user.email,
         updateAt: new Date().toISOString(),
         lastLoggedInTime: new Date().toISOString(),
@@ -33,7 +58,18 @@ export class userRepository {
       where: { email: user.email },
     });
   }
-
+  async create(user: Partial<User>) {
+    return this.prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        updateAt: new Date().toISOString(),
+        lastLoggedInTime: new Date().toISOString(),
+        password: user?.password,
+        role: user.role,
+      },
+    });
+  }
   async findById(id: number): Promise<User> {
     return this.prisma.user.findUnique({ where: { id } });
   }
