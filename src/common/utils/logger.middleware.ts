fb82eced381ee4e3a4d3d287e86aa31eb87846f7
alirezanqp <alirezanqp@gmail.com>
2023-03-26 18:20:58 +0330

    update logger

--- src/common/utils/logger.middleware.ts
+++ src/common/utils/logger.middleware.ts
@@ -2,8 +2,7 @@ import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
-export class 
-LoggerMiddleware implements NestMiddleware {
+export class LoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP');
 
   use(req: Request, res: Response, next: NextFunction): void {
