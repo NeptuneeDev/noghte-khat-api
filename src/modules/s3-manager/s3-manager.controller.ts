abe119331e30dff43623440c31ffd0fc9b6f7f46
alirezanqp <alirezanqp@gmail.com>
2023-03-26 13:52:49 +0330

    update and refactore s3 manager

--- src/modules/s3-manager/s3-manager.controller.ts
+++ src/modules/s3-manager/s3-manager.controller.ts
@@ -13,7 +13,6 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { File } from '../../common/interfaces/file.interface';
 import { ApiTags } from '@nestjs/swagger';
 import { deleteObject } from './dto/deleteObject.dto';
-import { Professor } from '@prisma/client';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { Role } from '../auth/types/roles.enum';
 @ApiTags('aws-s3')
@@ -23,30 +22,34 @@ export class S3ManagerController {
 
   @Roles(Role.Admin)
   @Get('buckets')
-  getBuckets(): Promise<any> {
-    return this.s3.listBuckets();
+  async getBuckets(): Promise<any> {
+    return await this.s3.listBuckets();
   }
 
   @Roles(Role.Admin)
   @Get('buckets/:bucketName')
-  getObjectsByBucket(@Param('bucketName') bucketName: string): Promise<any> {
-    return this.s3.listObjects(bucketName);
+  async getObjectsByBucket(
+    @Param('bucketName') bucketName: string,
+  ): Promise<any> {
+    return await this.s3.listObjects(bucketName);
   }
+
   @Roles(Role.Admin)
   @Post('buckets/:bucketName')
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(
+  async uploadFile(
     @Param('bucketName') bucketName: string,
     @UploadedFile() file: File,
   ): Promise<any> {
-    return this.s3.uploadFile(bucketName, file);
+    return await this.s3.uploadFile(bucketName, file);
   }
+
   @Roles(Role.Admin)
   @Delete('buckets/:bucketName')
-  deleteObject(
+  async deleteObject(
     @Param('bucketName') bucketName: string,
     @Body() body: deleteObject,
   ) {
-    return this.s3.deleteFile(bucketName, body.key);
+    return await this.s3.deleteFile(bucketName, body.key);
   }
 }
